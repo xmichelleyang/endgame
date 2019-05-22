641b5f29bc775d9b12398f1e95ec806777482a0d
gwagdanny <32524655+gwagdanny@users.noreply.github.com>
2019-05-22 05:20:48 -0700

    Overview + some minor errors

--- public/js/overview.js
+++ public/js/overview.js
@@ -26,23 +26,6 @@ $(document).ready(() => {
   // Define it as database
   const database = firebase.database();
 
-  // var cal = {
-  //   "Mon": [],
-  //   "Tue": [],
-  //   "Wed": [],
-  //   "Thu": [],
-  //   "Fri": [],
-  //   "Sat": [],
-  //   "Sun": [],
-  // };
-  cal['Mon'].push({
-    "Advil": "4pm"
-  });
-  // cal['Mon'].push({
-  //   "Tylenol": "5pm"
-  // });
-  // console.log(cal);
-
   database.ref("user_meds/").on("value", (snapshot) => {
     const allMedications = snapshot.val();
     if (allMedications) {
@@ -54,119 +37,57 @@ $(document).ready(() => {
             if (sched === "Monday") {
               database.ref("user_meds/" + med + "/" + sched).on("value", (snapshot) => {
                 const medTime = snapshot.val();
-                var newSched = {
-                  [med]: medTime.time
-                };
-                cal['Mon'].push(newSched);
-                $("#med-mon").append(`\n` + med + ` : ` + medTime.time);
-                // $("#med-monday").append(JSON.stringify(newSched));
-                // $("#user-med-schedule").append(`
-                // <h4 class="card-header"> Monday </h4>
-                //   ` + JSON.stringify(cal["Mon"]));
+                console.log(medTime);
+                $("#med-mon").append(`\n` + med + ` : ` + medTime);
               })
             }
             if (sched === "Tuesday") {
               database.ref("user_meds/" + med + "/" + sched).on("value", (snapshot) => {
                 const medTime = snapshot.val();
-                var newSched = {
-                  [med]: medTime.time
-                };
-                cal['Tue'].push(newSched);
-                $("#med-tue").append(`\n` + med + ` : ` + medTime.time);
+                $("#med-tue").append(`\n` + med + ` : ` + medTime);
               })
             }
             if (sched === "Wednesday") {
               database.ref("user_meds/" + med + "/" + sched).on("value", (snapshot) => {
                 const medTime = snapshot.val();
-                var newSched = {
-                  [med]: medTime.time
-                };
-                cal['Wed'].push(newSched);
-                $("#med-wed").append(`\n` + med + ` : ` + medTime.time);
+                $("#med-wed").append(`\n` + med + ` : ` + medTime);
               })
             }
             if (sched === "Thursday") {
               database.ref("user_meds/" + med + "/" + sched).on("value", (snapshot) => {
                 const medTime = snapshot.val();
-                var newSched = {
-                  [med]: medTime.time
-                };
-                cal['Thu'].push(newSched);
-                $("#med-thu").append(`\n` + med + ` : ` + medTime.time);
+                $("#med-thu").append(`\n` + med + ` : ` + medTime);
               })
             }
             if (sched === "Friday") {
               database.ref("user_meds/" + med + "/" + sched).on("value", (snapshot) => {
                 const medTime = snapshot.val();
-                var newSched = {
-                  [med]: medTime.time
-                };
-                cal['Fri'].push(newSched);
-                $("#med-fri").append(`\n` + med + ` : ` + medTime.time);
+                $("#med-fri").append(`\n` + med + ` : ` + medTime);
               })
             }
             if (sched === "Saturday") {
               database.ref("user_meds/" + med + "/" + sched).on("value", (snapshot) => {
                 const medTime = snapshot.val();
-                var newSched = {
-                  [med]: medTime.time
-                };
-                cal['Sat'].push(newSched);
-                $("#med-sat").append(`\n` + med + ` : ` + medTime.time);
+                $("#med-sat").append(`\n` + med + ` : ` + medTime);
               })
             }
             if (sched === "Sunday") {
               database.ref("user_meds/" + med + "/" + sched).on("value", (snapshot) => {
                 const medTime = snapshot.val();
-                var newSched = {
-                  [med]: medTime.time
-                };
-                cal['Sun'].push(newSched);
-                $("#med-sun").append(`\n` + med + ` : ` + medTime.time);
+                $("#med-sun").append(`\n` + med + ` : ` + medTime);
 
               })
             }
           })
         });
       })
-
-
     }
-
   });
-  // $("#med-today").append("aaaa");
 
   document.addEventListener('DOMContentLoaded', function() {
     $("#med-today").append("aa");
    // your code here
   }, false);
-
-  // $("#med-today").append(cal['Sun']);
-  console.log(cal);
-  // console.log(cal["Mon"]);
-
-  // async function f() {
-  //   // waits for 1 second, then result becomes 2
-  //   let result = await new Thenable(5);
-  //   alert(result);
-  // }
-
-
-  // $("#user-med-schedule").append(`
-  // <h4 class="card-header"> Monday </h4>
-  //   ` + JSON.stringify(cal["Mon"]));
-  // JSON.stringify(cal["Mon"]))
-  // $.ajax({
-  // 	url: 'data',
-  // 	type: 'GET',
-  // 	dataType: 'json',
-  // 	success: (data) => {
-  // 		console.log("Ajax successful", data );
-  // 		$("#user-name").html("<p>" + data["name"] + "</p>");
-  // 		$("#user-info").html("<p><strong>Medications: </strong><br>" + data["medications"] + "</p>");
-  //
-  // 	}
-  // });
 
   // David - Pressing Pill Button
   $(".day-button").click(function() {
